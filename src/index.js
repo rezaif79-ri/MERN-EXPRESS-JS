815f50b92e126b5fb130fe44e2bec19e17d1de85
rezaif79 <rezaif79.ri@gmail.com>
2021-07-11 19:30:42 +0700

    First Update (frontend staticfiles and backend api)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const { pathToFileURL } = require('url');
+const path = require('path');
 
 const app = express();
+const router = express.Router();
+const parent = path.resolve(__dirname, '..');
 
-app.use(() => {
-    console.log("Server starting...");
+router.use('/products', (req, res, next) => {
+    console.log('request: ', req.originalUrl);
+    console.log('method: ', req.method);
+    res.json({
+        'url':req.originalUrl,
+        'method': req.method,
+        'item':'Tas Mahal',
+
+    });
+    next();
+})
+
+router.use('/home', (req, res, next) => {
+    res.sendFile(parent +'/template/index.html');
+    console.log(path.resolve(parent +'/template/index.html'));
+
 });
+
+
+app.use('/', router);
 
 app.listen(4000);
